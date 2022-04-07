2b896f3af2c847ab49bad822f25a6d14447da290
Tyrone Kong <tyronekong@gmail.com>
2022-04-07 15:54:29 +0100

    applied different login and logout icon

--- my-vite-app/src/components/loginButton.jsx
+++ my-vite-app/src/components/loginButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "@mui/material/Button";
-
+import LoginIcon from "@mui/icons-material/Login";
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
   return (
@@ -9,6 +9,7 @@ const LoginButton = () => {
       className="btn btn-primary btn-block"
       onClick={() => loginWithRedirect()}
     >
+      <LoginIcon />
       Log In
     </Button>
   );
--- my-vite-app/src/components/logoutButton.jsx
+++ my-vite-app/src/components/logoutButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import Button from "@mui/material/Button";
-
+import LogoutIcon from "@mui/icons-material/Logout";
 const LogoutButton = () => {
   const { logout } = useAuth0();
   return (
@@ -12,6 +12,7 @@ const LogoutButton = () => {
         })
       }
     >
+      <LogoutIcon />
       Log Out
     </Button>
   );
